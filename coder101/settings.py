cd1bbd3210f038f57e69f0ce5388ba720f5306ce
Yohith-Programmer101 <75368049+Yohith-Programmer101@users.noreply.github.com>
2021-02-23 19:04:58 +0530

    Getting started

--- coder101/settings.py
+++ coder101/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = 'c0c^i^uiy2qh4#egh=)_#svp+spd+i&rnrbwq0z#@(^$opt#*b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
